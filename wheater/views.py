c3a7bdaba03182addc3af1ef8b1cd9a960943e5c
pauloestrella <pauloestrella1994@gmail.com>
2021-10-02 20:36:00 -0300

    Add wheater view

--- wheater/views.py
+++ wheater/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import serializers
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Wheater
+from .serializers import WheaterSerializer
 
-# Create your views here.
+
+class WheaterView(APIView):
+    def get(self, request):
+        wheater = Wheater.objects.all()
+        serializer = WheaterSerializer(wheater, many=True)
+        return Response(serializer.data)
+
