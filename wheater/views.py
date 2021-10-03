c98d2419c193b99df1e759157e1a060cde2334d0
pauloestrella <pauloestrella1994@gmail.com>
2021-10-02 22:10:39 -0300

    Add response status 200 in get

--- wheater/views.py
+++ wheater/views.py
@@ -1,13 +1,15 @@
 from rest_framework import serializers
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 from .models import Wheater
 from .serializers import WheaterSerializer
 
 
 class WheaterView(APIView):
+
     def get(self, request):
         wheater = Wheater.objects.all()
         serializer = WheaterSerializer(wheater, many=True)
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
