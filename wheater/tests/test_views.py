e2cf569da202bc8e566773ab1f6c304870b5f5f7
pauloestrella <pauloestrella1994@gmail.com>
2021-10-03 15:54:41 -0300

    Add test for query string parameter date

--- wheater/tests/test_views.py
+++ wheater/tests/test_views.py
@@ -1,13 +1,9 @@
 import pytest
-from _pytest.python import pytest_generate_tests
-from django.http import response
-from django.test import RequestFactory, TestCase
+from django.test import TestCase
 from django.urls import reverse
 from mixer.backend.django import mixer
 from rest_framework.test import APIClient
 from wheater.models import Wheater
-from wheater.tests.conftest import wheater_one
-from wheater.views import WheatersView, WheaterView
 
 
 class TestWheatersView(TestCase):
@@ -38,11 +34,36 @@ class TestWheatersView(TestCase):
         assert response.status_code == 200
         assert response.json()[0] == response_data
 
-
+    def test_wheater_view_with_date_param(self):
+        path = reverse('wheater')
+        mixer.blend(
+            Wheater,
+            date="2019-06-11",
+            lat=41.8818,
+            lon=-87.6231,
+            city="Chicago",
+            state="Illinois",
+            temperatures=[24.0, 21.5, 24.0, 19.5, 25.5]
+        )
+        response = self.client.get(path + '?date=2019-06-11')
+        response_data = {
+            "id": 1,
+            "date": "2019-06-11",
+            "lat": 41.8818,
+            "lon": -87.6231,
+            "city": "Chicago",
+            "state": "Illinois",
+            "temperatures": [24.0, 21.5, 24.0, 19.5, 25.5]
+        }
+        assert response.status_code == 200
+        assert response.json()[0] == response_data
 
 
 
 @pytest.mark.django_db
 class TestWheaterView:
+    def setUp(self):
+        self.client = APIClient()
+
     def test_wheaterbyid_view(self):
         pass
