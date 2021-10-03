f5f8b503da3014fa3ab27005f15dd14fc135400a
pauloestrella <pauloestrella1994@gmail.com>
2021-10-03 12:04:49 -0300

    Customize filters for querystrings

--- wheater/views.py
+++ wheater/views.py
@@ -1,15 +1,30 @@
 from rest_framework import serializers
-from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics
 from .models import Wheater
 from .serializers import WheaterSerializer
 
 
 class WheatersView(generics.ListCreateAPIView):
-    queryset = Wheater.objects.all()
     serializer_class = WheaterSerializer
-    filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['city', 'date']
+
+    def get_queryset(self):
+        city = self.request.query_params.get('city', None)
+        date = self.request.query_params.get('date', None)
+        sort = self.request.query_params.get('sort', None)
+        queryset = Wheater.objects.all()
+
+        if city:
+            data = city.split(",")
+            queryset = queryset.filter(city__in=data)
+
+        if date:
+            queryset = queryset.filter(date=date)
+        
+        if sort:
+            queryset = queryset.filter().order_by(sort)
+        
+        return queryset
+        
 
 class WheaterView(generics.RetrieveAPIView):
     queryset = Wheater.objects.all()
