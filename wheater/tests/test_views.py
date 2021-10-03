94f0c2ddd1e999819e02659e5dc0f1c77cc77b18
pauloestrella <pauloestrella1994@gmail.com>
2021-10-03 16:22:35 -0300

    Add test to query params and post method

--- wheater/tests/test_views.py
+++ wheater/tests/test_views.py
@@ -58,7 +58,132 @@ class TestWheatersView(TestCase):
         assert response.status_code == 200
         assert response.json()[0] == response_data
 
+    def test_wheater_view_with_city_param(self):
+        path = reverse('wheater')
+        mixer.blend(
+            Wheater,
+            date="2019-06-11",
+            lat=41.8818,
+            lon=-87.6231,
+            city="Chicago",
+            state="Illinois",
+            temperatures=[24.0, 21.5, 24.0, 19.5, 25.5]
+        )
+        response = self.client.get(path + '?city=Chicago')
+        response_data = {
+            "id": 1,
+            "date": "2019-06-11",
+            "lat": 41.8818,
+            "lon": -87.6231,
+            "city": "Chicago",
+            "state": "Illinois",
+            "temperatures": [24.0, 21.5, 24.0, 19.5, 25.5]
+        }
+        assert response.status_code == 200
+        assert response.json()[0] == response_data
 
+    def test_wheater_view_with_city_and_date_param(self):
+        path = reverse('wheater')
+        mixer.blend(
+            Wheater,
+            date="2019-06-11",
+            lat=41.8818,
+            lon=-87.6231,
+            city="Chicago",
+            state="Illinois",
+            temperatures=[24.0, 21.5, 24.0, 19.5, 25.5]
+        )
+        response = self.client.get(path + '?city=Chicago&date=2019-06-11')
+        response_data = {
+            "id": 1,
+            "date": "2019-06-11",
+            "lat": 41.8818,
+            "lon": -87.6231,
+            "city": "Chicago",
+            "state": "Illinois",
+            "temperatures": [24.0, 21.5, 24.0, 19.5, 25.5]
+        }
+        assert response.status_code == 200
+        assert response.json()[0] == response_data
+
+    def test_wheater_view_with_sort_ordering_by_date_asc(self):
+        path = reverse('wheater')
+        mixer.blend(
+            Wheater,
+            date="2019-06-11",
+            lat=41.8818,
+            lon=-87.6231,
+            city="Chicago",
+            state="Illinois",
+            temperatures=[24.0, 21.5, 24.0, 19.5, 25.5]
+        )
+        mixer.blend(
+            Wheater,
+            date="2020-06-11",
+            lat=41.8818,
+            lon=-87.6231,
+            city="Curitiba",
+            state="Paraná",
+            temperatures=[24.0, 21.5, 24.0, 19.5, 25.5]
+        )
+        response = self.client.get(path + '?sort=date')
+        first_date = {"date": "2019-06-11"}
+        second_date = {"date": "2020-06-11"}
+
+        assert response.status_code == 200
+        assert response.json()[0]["date"] == first_date["date"]
+        assert response.json()[1]["date"] == second_date["date"]
+
+    def test_wheater_view_with_sort_ordering_by_date_desc(self):
+        path = reverse('wheater')
+        mixer.blend(
+            Wheater,
+            date="2019-06-11",
+            lat=41.8818,
+            lon=-87.6231,
+            city="Chicago",
+            state="Illinois",
+            temperatures=[24.0, 21.5, 24.0, 19.5, 25.5]
+        )
+        mixer.blend(
+            Wheater,
+            date="2020-06-11",
+            lat=41.8818,
+            lon=-87.6231,
+            city="Curitiba",
+            state="Paraná",
+            temperatures=[24.0, 21.5, 24.0, 19.5, 25.5]
+        )
+        response = self.client.get(path + '?sort=-date')
+        last_date = {"date": "2020-06-11"}
+        first_date = {"date": "2019-06-11"}
+
+        assert response.status_code == 200
+        assert response.json()[0]["date"] == last_date["date"]
+        assert response.json()[1]["date"] == first_date["date"]
+
+    def test_wheater_view_post(self):
+        path = reverse('wheater')
+        wheater = {
+            "date": "2019-06-11",
+            "lat": 41.8818,
+            "lon": -87.6231,
+            "city": "Chicago",
+            "state": "Illinois",
+            "temperatures": 25.5
+        }
+        response = self.client.post(path, data=wheater)
+        response_data = {
+            "id": 1,
+            "date": "2019-06-11",
+            "lat": 41.8818,
+            "lon": -87.6231,
+            "city": "Chicago",
+            "state": "Illinois",
+            "temperatures": 25.5
+        }
+        assert response.status_code == 201
+        assert response.json() == response_data
 
 @pytest.mark.django_db
 class TestWheaterView:
